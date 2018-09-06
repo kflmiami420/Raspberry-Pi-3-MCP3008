b7a1d959a6093fb1601ce442f4fb79f50438b796
kflmiami420 <42599019+kflmiami420@users.noreply.github.com>
2018-09-05 23:44:35 -0400

    Create ledmcp3008.py
    The pin1 of the MCP3008 starts from below the half circle you can see in the pin out diagram. MCP3008 ADC has a total of 16 pins out of which 8 pins are for taking the analog input. The analog input pins are from CH0-CH7 (Pins 1-8). On the other side, we have different pins which are as follows
    
    DGND is digital ground pin for the chip.
    
    CS is the chip select pin.
    
    DIN is the data input pin from the Raspberry Pi.
    
    DOUT is the data output pin.
    
    CLK is the clock pin.
    
    AGND is the analog ground pin.
    
    VREF is the analog reference voltage. Connect to 3.3V. You can change it if you want to change the scale.
    
    VDD is the power pin for the chip.
    
    
    After that, connect the center of the 10k Potentiometer to the CH0 of the MCP3008 and connect the other two ends of Potentiometer to the 3.3V and GND. In the end connect the positive lead of LED which is the longer leg of LED to the GPIO 20 of the Raspberry Pi and then connect the 220 ohm resistor from the negative lead of Raspberry Pi to the GND of Raspberry Pi.
    
    MCP3008	Raspberry Pi
    VDD	3.3V
    VREF	3.3V
    AGND	GND
    CLK	GPIO11
    DOUT	GPIO9
    DIN	GPIO10
    CS	GPIO8
    DGND	GND

---
+++ ledmcp3008.py
@@ -0,0 +1,25 @@
+# Importing modules
+import spidev # To communicate with SPI devices
+from numpy import interp	# To scale values
+from time import sleep	# To add delay
+import RPi.GPIO as GPIO	# To use GPIO pins
+# Start SPI connection
+spi = spidev.SpiDev() # Created an object
+spi.open(0,0)	
+# Initializing LED pin as OUTPUT pin
+led_pin = 20
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+# Creating a PWM channel at 100Hz frequency
+pwm = GPIO.PWM(led_pin, 100)
+pwm.start(0) 
+# Read MCP3008 data
+def analogInput(channel):
+  adc = spi.xfer2([1,(8+channel)<<4,0])
+  data = ((adc[1]&3) << 8) + adc[2]
+  return data
+while True:
+	output = analogInput(0) # Reading from CH0
+	output = interp(output, [0, 1023], [0, 100])
+  	pwm.ChangeDutyCycle(output)
+  	sleep(0.1)
